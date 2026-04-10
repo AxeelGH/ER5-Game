92fd6f6d0568b90cf7d1ec4e75a0878f8df55704
yonederzq <yonederzq@gmail.com>
2026-04-10 16:26:51 +0200

    Cambio en el archivo frames

--- ER5-Game/src/Frames.js
+++ ER5-Game/src/Frames.js
@@ -7,4 +7,24 @@ export default class Frames{
         this.speed = speed;                     // Speed of animation
         this.frameChangeCounter = 0;            // Counter to control frame change timing
     }
+
+    update() {
+        this.frameChangeCounter++;
+        
+        if (this.frameChangeCounter >= this.speed) {
+            this.frameChangeCounter = 0;
+            this.frameCounter = (this.frameCounter + 1) % this.framesPerState;
+        }
+        
+        return this.frameCounter;
+    }
+
+    reset() {
+        this.frameCounter = 0;
+        this.frameChangeCounter = 0;
+    }
+
+    getCurrentFrame() {
+        return this.frameCounter;
+    }
 }
