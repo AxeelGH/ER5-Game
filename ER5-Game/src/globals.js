94e5e34ec14aea3640cde23126a434b7f8247569
itziar <itz.ormazabal@gmail.com>
2026-04-10 10:47:29 +0200

    feat: added globals file

--- ER5-Game/src/globals.js
+++ ER5-Game/src/globals.js
@@ -2,7 +2,7 @@ export default{
 
     canvas: {},
     ctx: {},
-    previousCycleMilliseconds: 0,
+    previousCycleMilliseconds: -1,
     deltaTime: 0,
     cycleRealTime: 0,
     frameTimeObj: 0,
