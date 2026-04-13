5399e11db49e08d45bf0e081617c92e7312e6bc1
yonederzq <yonederzq@gmail.com>
2026-04-13 21:34:14 +0200

    Added more solid tiles

--- src/CollisionManager.js
+++ src/CollisionManager.js
@@ -6,7 +6,7 @@ export default class CollisionManager {
     
     static getSolidTileIds() {
         return [
-            18,34,35,36,38,42,43,44,68,69,70,71,72,75,76,77,78,79
+            30,31,32,34,35,36,38,42,43,44,68,69,70,71,72,75,76,77,78,79
         ];
     }
     
