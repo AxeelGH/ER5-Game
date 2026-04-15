e7e25f79a452d27164534c855012580d64a0d312
itziar <itz.ormazabal@gmail.com>
2026-04-15 23:20:16 +0200

    feat: add game factory

--- src/GameFactory.js
+++ src/GameFactory.js
@@ -1,5 +1,14 @@
+import { Game } from "./game.js";
+
 export default class GameFactory {
 
-    
+    constructor(data) {
+        this.gameData = data;
+    }
+
+    create(canvas) {
+        
+        return Game.create(canvas, this.gameData);
+    } 
 
 }
